b724e585a50f85cc757c8e3b07d2b9abfe3d38a5
yushicheng <1542874601@qq.com>
2022-08-04 14:18:48 +0800

    temp:临时节点标记 星期四 下午 2:18:47 八月 4日 2022 修改了 .gitignore 修改了 electron/ipcMain.js 修改了 package.json 删除了 .npmrc

--- electron/ipcMain.js
+++ electron/ipcMain.js
@@ -2,11 +2,12 @@ import path from "path";
 import { ipcMain } from "electron";
 import { exec } from "child_process";
 
-const test_file_path = path.resolve(__dirname, "../statics/dist/json_demo");
+const python_runtime_path = path.resolve(__dirname, "../statics/30912/bin/python3");
+const python_file_path = path.resolve(__dirname, "../statics/json_demo.py");
 
 ipcMain.handle("test", async (event, args) => {
   const result = await new Promise((resolve, reject) => {
-    exec(test_file_path, (error, stdout, stderr) => {
+    exec(`${python_runtime_path} ${python_file_path}`, (error, stdout, stderr) => {
       if (error) {
         return reject(error);
       };
